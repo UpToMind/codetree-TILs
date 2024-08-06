908dbd0c0441f918cecfdb2bab59bc94763a8ae8
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-08-06 22:11:31 +0900

    Title: 중앙값 구하기, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240806/중앙값 구하기/find-the-median.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+if (a>b) {
+    if (b > c) console.log(b);
+    else {
+        if (a > c) console.log(c);  
+        else console.log(a);
+    }
+}
+
+else {
+    if (a > c) console.log(a);
+    else {
+        if (c > b) console.log(b);  
+        else console.log(c);
+    }
+}
