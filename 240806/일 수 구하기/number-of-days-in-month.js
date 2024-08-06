f0222b0eb133967bc758b1ba33469aea0d405368
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-08-06 21:46:28 +0900

    Title: 일 수 구하기, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

--- 240806/일 수 구하기/number-of-days-in-month.js
+++ 240806/일 수 구하기/number-of-days-in-month.js
@@ -1,20 +1,23 @@
+// 입력 및 변수 선언
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split("\n");
+let n = Number(fs.readFileSync(0).toString().trim());
 
-let mon = Number(input[0]);
-
-if (mon % 2 === 1) {
-    console.log(31);
+// 출력
+if (n === 2) {
+    console.log(28);
 }
-
-else {
-    if(mon === 2) {
-        console.log(28);
-    }
-    else if (mon === 8) {
+else if (n <= 7) {
+    if (n % 2 === 1) {
         console.log(31);
     }
-
+    else {
+        console.log(30);
+    }
+}
+else {
+    if (n % 2 === 0) {
+        console.log(31);
+    }
     else {
         console.log(30);
     }
