ba800dda59ccfa1ee0e9a04148e1ebb610e23889
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-08-07 02:15:15 +0900

    Title: a 부터 b 까지, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240807/a 부터 b 까지/a-to-b.js
+++ 240807/a 부터 b 까지/a-to-b.js
@@ -8,13 +8,12 @@ let result = a;
 while( a < b ) {
     if(a%2 != 0) {
         a *= 2;
-        result += " " + a;
+        
     }
     else {
         a += 3;
-        result += " " + a;
     }
-    
+    result += " " + a;
 }
 
 console.log(result);
