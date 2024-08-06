b7f360fa5673a0cb1fc63e0756d348308c9cff3f
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-08-06 21:19:31 +0900

    Title: 남녀노소 구분짓기, Time: 224ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240806/남녀노소 구분짓기/sex-and-age.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let sex = Number(input[0]);
+let age = Number(input[1]);
+
+if (sex === 0) {
+    if (age >= 19) {
+        console.log("MAN");
+    }
+    else {
+        console.log("BOY");
+    }
+}
+
+else {
+    if (age >= 19) {
+        console.log("WOMAN");
+    }
+    else {
+        console.log("GIRL");
+    }
+}
