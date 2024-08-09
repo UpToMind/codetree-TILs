06bd57ab8bbf2a29bb488be656c147ae84be4d08
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-08-09 20:39:03 +0900

    Title: 10진수로 변환하기, Time: 228ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240809/10진수로 변환하기/convert-to-decimal.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split('').map(Number);
+let num = 0;
+
+for(i=0; i< input.length; i++) {
+    num = num * 2 + input[i];
+}
+
+console.log(num);
