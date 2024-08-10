95a1e13e1a7ccb11214a68e0b952b8f9193079b8
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-08-11 05:30:35 +0900

    Title: 여러가지 진수변환, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240811/여러가지 진수변환/various-numeral-system-transformations.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let n = Number(input[0]);
+let b = Number(input[1]);
+let result = [];
+let num = "";
+
+while (true) {
+    if(n <= 1) {
+        result.push(n);
+        break;
+    }
+
+    result.push(n % b);
+    n = Math.floor(n / b);
+
+}
+
+for(let i = result.length-1 ; i >= 0; i--) {
+    num += result[i];
+}
+
+console.log(num);
