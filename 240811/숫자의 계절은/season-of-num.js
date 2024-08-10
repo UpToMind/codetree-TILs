b13f420778a833455f6a31b70f16c909ac61ebbf
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-08-11 04:29:30 +0900

    Title: 숫자의 계절은, Time: 176ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240811/숫자의 계절은/season-of-num.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+if(n >= 12 || n <= 2) console.log("Winter");
+else if(n <= 5) console.log("Spring");
+else if(n <= 8) console.log("Summer");
+else console.log("Fall")
