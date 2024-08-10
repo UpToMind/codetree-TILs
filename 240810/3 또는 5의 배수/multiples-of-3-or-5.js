a447e9570c23c0db2c381aa10e3188344ffcee10
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-08-10 14:10:01 +0900

    Title: 3 또는 5의 배수, Time: 173ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240810/3 또는 5의 배수/multiples-of-3-or-5.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input);
+
+if(a%3 == 0) console.log("YES");
+else console.log("NO");
+
+if(a%5 == 0) console.log("YES");
+else console.log("NO");
