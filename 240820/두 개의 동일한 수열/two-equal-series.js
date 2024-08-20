109ea128587bd9ba83cfd9f3e760b871b82ceffa
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-08-20 16:12:11 +0900

    Title: 두 개의 동일한 수열, Time: 228ms, Memory: 47MB, Status: Passed - CodeTree

--- 240820/두 개의 동일한 수열/two-equal-series.js
+++ 240820/두 개의 동일한 수열/two-equal-series.js
@@ -6,8 +6,8 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
 let n = Number(input[0]);
-let arrA = input[1].split(" ").map(Number);
-let arrB = input[2].split(" ").map(Number);
+let arrA = input[1].trim().split(" ").map(Number);
+let arrB = input[2].trim().split(" ").map(Number);
 
 arrA.sort(cmp);
 arrB.sort(cmp);
