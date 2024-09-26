041ced6452c3f71cd220969e090d768a659741e7
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-09-26 21:52:20 +0900

    Title: k번째로 신기한 문자열, Time: 227ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240926/k번째로 신기한 문자열/kth-special-string.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let [n,k,t] = input[0].split(" ");
+n = Number(n);
+k = Number(k);
+
+let arr = [];
+let cnt = 0;
+
+for(let i = 0; i < n; i++) {
+    arr.push(input[i+1]);
+}
+arr.sort();
+
+for(let i = 0; i< n; i++) {
+    if(arr[i].includes(t)) cnt++;
+    if(cnt === k) {
+        console.log(arr[i]);
+        
+    }
+}
