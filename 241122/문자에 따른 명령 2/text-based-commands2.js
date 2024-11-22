1cd3bd2886e556f090cee792fd41f0518e6f55d5
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-11-22 18:11:34 +0900

    Title: 문자에 따른 명령 2, Time: 186ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241122/문자에 따른 명령 2/text-based-commands2.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("");
+const dx = [1, 0, -1, 0];
+const dy = [0, -1, 0, 1];
+let startingPoint = [0,0];
+let dirNum = 3;
+
+for(let i=0; i < input.length; i++) {
+    if(input[i] === "L"){
+        dirNum = (dirNum - 1) % 4;
+    }
+
+    else if (input[i] === "R") {
+        dirNum = (dirNum + 1) % 4;
+    }
+
+    else if (input[i] === "F") {
+        startingPoint[0] += dx[dirNum];
+        startingPoint[1] += dy[dirNum];
+    }
+}
+
+console.log(startingPoint[0], startingPoint[1]);
