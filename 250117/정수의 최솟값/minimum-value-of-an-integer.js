6f43ee170920fff97dc2b3ae08d5d1df7a99706b
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-01-16 15:49:46 -0800

    Title: 정수의 최솟값, Time: 178ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250117/정수의 최솟값/minimum-value-of-an-integer.js
@@ -0,0 +1,16 @@
+function findMin(a,b,c) {
+    if ( a>= b) {
+        if (b <= c) return b;
+        else return c;
+    }
+
+    else {
+        if (a <= c) return a;
+        else return c;
+    }
+}
+
+const fs = require("fs");
+let [a,b,c] = fs.readFileSync(0).toString().trim().split(" ");
+
+console.log(findMin(a,b,c));
